8f9a65eda0b2dd710781210e078ad9d8de9d53f0
srivarshinichowdary-source <srivarshinichowdary@gmail.com>
2026-05-04 14:36:18 +0530

    Created basic project structure

---
+++ DocterAppointment-backend/src/test/java/com/task/DocterAppointment/DocterAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.DocterAppointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocterAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
