c862272b96910ac6c69b062f790b8814a5de5274
srivarshinichowdary-source <srivarshinichowdary@gmail.com>
2026-05-05 15:42:43 +0530

    Added JWT security utilities

---
+++ DocterAppointment-backend/src/main/java/com/task/DocterAppointment/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.task.DocterAppointment.security;
+
+import com.task.DocterAppointment.entity.User;
+import com.task.DocterAppointment.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
